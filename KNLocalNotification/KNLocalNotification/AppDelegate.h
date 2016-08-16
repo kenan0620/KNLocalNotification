44090aa22d0065a6ce5fdc99e72a2bbf652d1954
kenan0620 <houkenan0620@126.com>
2016-08-16 10:04:10 +0800

    0816、本地推送消息的实现

---
+++ KNLocalNotification/KNLocalNotification/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KNLocalNotification
+//
+//  Created by 柯南 on 16/8/16.
+//  Copyright © 2016年 柯南. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KNLocalNotification/KNLocalNotification/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KNLocalNotification
+//
+//  Created by 柯南 on 16/8/16.
+//  Copyright © 2016年 柯南. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
